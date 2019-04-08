67af5bc319fec7874f8fdc0dbae3a86ed8016684
Usuario <Usuario@DESKTOP-PVMAU3S>
2019-04-08 18:41:06 -0500

    interfaces

---
+++ src/ec/ecu/ups/clases/Animal.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ecu.ups.clases;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Animal {
+    
+    private String nombre;
+    private int edad;
+    private double peso;
+    private String sexo;
+
+    public Animal(String nombre, int edad, double peso, String sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    
+    
+}
---
+++ src/ec/ecu/ups/clases/Ave.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.ecu.ups.clases;
+
+import ec.ecu.ups.interfaces.Acciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ave extends Animal implements Acciones{
+
+    public Ave(String nombre, int edad, double peso, String sexo) {
+        super(nombre, edad, peso, sexo);
+    }
+
+    @Override
+    public void comer() {
+        
+    }
+
+    @Override
+    public void dormir() {
+        
+    }
+
+    @Override
+    public void moverse() {
+        
+    }
+
+    @Override
+    public void jugar() {
+        
+    }
+
+    @Override
+    public void reproducirse() {
+        
+    }
+    
+}
